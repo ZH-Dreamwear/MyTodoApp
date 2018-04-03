719fc992e95ca49c8b8a5a8bf1d63495060c120a
ZH-Dreamwear <1217243450@qq.com>
2018-04-04 02:01:32 +0800

    项目整体架构搭建与样式实现

---
+++ js/base.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Administrator
+* @Date:   2018-04-03 23:23:43
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-04-03 23:29:48
+*/
+;(function(){
+	'use strict';
+	console.log("test");
+})();
